f049330f6a289a5c2175c2d79c2ec3aca46f8ab4
SmailAitHemmou <aithemmou@gmail.com>
2025-10-07 19:41:47 +0100

    Create CRUD For Movies And Connetact With Templates

--- movies/urls.py
+++ movies/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
-from .views import movies
+from .views import MovieList, MovieDetail, MovieCreate, MovieUpdate, MovieDelete
 
 urlpatterns = [
-    path('', movies, name='movies')
+    path('', MovieList.as_view(), name='movies'),
+    path('<int:pk>/', MovieDetail.as_view(), name='movie_detail'),
+    path('create/', MovieCreate.as_view(), name='movie_create'),
+    path('update/<int:pk>/', MovieUpdate.as_view(), name='movie_update'),
+    path('delete/<int:pk>/', MovieDelete.as_view(), name='movie_delete'),
+
 ]
--- movies/views.py
+++ movies/views.py
@@ -1,4 +1,33 @@
 from django.shortcuts import render
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from .models import Movie
 
-def movies(request):
-    return render(request, 'movies/movies.html')
+
+class MovieList(ListView):
+    model = Movie
+    context_object_name = 'movies'
+
+class MovieDetail(DetailView):
+    model = Movie
+    context_object_name = 'movie'
+    template_name = 'movies/movie_detail.html'
+
+class MovieCreate(CreateView):
+    model = Movie
+    fields = '__all__'
+    success_url = reverse_lazy('movies')
+
+class MovieUpdate(UpdateView):
+    model = Movie
+    fields = '__all__'
+    success_url = reverse_lazy('movies')
+
+class MovieDelete(DeleteView):
+    model = Movie
+    context_object_name = 'movie'
+    success_url = reverse_lazy('movies')
+    template_name = 'movies/movie_delete.html'
+
