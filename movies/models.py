406366e2f760d46645ea40bf6a95fd5661365fd9
SmailAitHemmou <aithemmou@gmail.com>
2025-10-07 17:08:26 +0100

    Create Models to Movies And Admin Panal (superuser).

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Movie(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    name_movie = models.CharField(max_length=200)
+    release_year = models.IntegerField(null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
+    complete = models.BooleanField(default=False)
+    crested = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name_movie
+    
+    class Meta:
+        ordering = ['complete']
+    
+
+
